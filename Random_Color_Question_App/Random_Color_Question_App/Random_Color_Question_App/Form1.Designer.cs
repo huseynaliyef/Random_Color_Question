5a2e5ea581f95b5b093d1900ff9da74ca63823f5
huseynaliyef <huseyn.064.064@gmail.com>
2022-10-08 21:06:21 +0400

    Random_Color_Question

---
+++ Random_Color_Question_App/Random_Color_Question_App/Random_Color_Question_App/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Random_Color_Question_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Question_box = new System.Windows.Forms.TextBox();
+            this.aVarianti = new System.Windows.Forms.Button();
+            this.bVarianti = new System.Windows.Forms.Button();
+            this.cVarianti = new System.Windows.Forms.Button();
+            this.dVarianti = new System.Windows.Forms.Button();
+            this.eVarianti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Question_box
+            // 
+            this.Question_box.Enabled = false;
+            this.Question_box.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Question_box.Location = new System.Drawing.Point(177, 98);
+            this.Question_box.Multiline = true;
+            this.Question_box.Name = "Question_box";
+            this.Question_box.Size = new System.Drawing.Size(425, 55);
+            this.Question_box.TabIndex = 0;
+            this.Question_box.Text = "Which Color is the background ?";
+            // 
+            // aVarianti
+            // 
+            this.aVarianti.Location = new System.Drawing.Point(46, 244);
+            this.aVarianti.Name = "aVarianti";
+            this.aVarianti.Size = new System.Drawing.Size(75, 23);
+            this.aVarianti.TabIndex = 1;
+            this.aVarianti.Text = "A)";
+            this.aVarianti.UseVisualStyleBackColor = true;
+            this.aVarianti.Click += new System.EventHandler(this.duymeler);
+            // 
+            // bVarianti
+            // 
+            this.bVarianti.Location = new System.Drawing.Point(177, 244);
+            this.bVarianti.Name = "bVarianti";
+            this.bVarianti.Size = new System.Drawing.Size(75, 23);
+            this.bVarianti.TabIndex = 2;
+            this.bVarianti.Text = "B)";
+            this.bVarianti.UseVisualStyleBackColor = true;
+            this.bVarianti.Click += new System.EventHandler(this.duymeler);
+            // 
+            // cVarianti
+            // 
+            this.cVarianti.Location = new System.Drawing.Point(335, 244);
+            this.cVarianti.Name = "cVarianti";
+            this.cVarianti.Size = new System.Drawing.Size(75, 23);
+            this.cVarianti.TabIndex = 3;
+            this.cVarianti.Text = "C)";
+            this.cVarianti.UseVisualStyleBackColor = true;
+            this.cVarianti.Click += new System.EventHandler(this.duymeler);
+            // 
+            // dVarianti
+            // 
+            this.dVarianti.Location = new System.Drawing.Point(500, 244);
+            this.dVarianti.Name = "dVarianti";
+            this.dVarianti.Size = new System.Drawing.Size(75, 23);
+            this.dVarianti.TabIndex = 4;
+            this.dVarianti.Text = "D)";
+            this.dVarianti.UseVisualStyleBackColor = true;
+            this.dVarianti.Click += new System.EventHandler(this.duymeler);
+            // 
+            // eVarianti
+            // 
+            this.eVarianti.Location = new System.Drawing.Point(652, 244);
+            this.eVarianti.Name = "eVarianti";
+            this.eVarianti.Size = new System.Drawing.Size(75, 23);
+            this.eVarianti.TabIndex = 5;
+            this.eVarianti.Text = "E)";
+            this.eVarianti.UseVisualStyleBackColor = true;
+            this.eVarianti.Click += new System.EventHandler(this.duymeler);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(792, 398);
+            this.Controls.Add(this.eVarianti);
+            this.Controls.Add(this.dVarianti);
+            this.Controls.Add(this.cVarianti);
+            this.Controls.Add(this.bVarianti);
+            this.Controls.Add(this.aVarianti);
+            this.Controls.Add(this.Question_box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox Question_box;
+        private Button aVarianti;
+        private Button bVarianti;
+        private Button cVarianti;
+        private Button dVarianti;
+        private Button eVarianti;
+    }
+}
---
+++ Random_Color_Question_App/Random_Color_Question_App/Random_Color_Question_App/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System.Media;
+using System.Windows.Forms;
+
+namespace Random_Color_Question_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.get_question();
+        }
+
+        private void get_question()
+        {
+            Color1 c = new Color1();
+            this.BackColor = c.change_color();
+            List<string> values = new List<string>() { "Blue", "Black", "Red", "White", "Yellow" };
+
+            Random rnd = new Random();
+            var ran_duz = values.OrderBy(_ => rnd.Next()).ToList();
+            for (int i = 0; i < ran_duz.Count; i++)
+            {
+                Color1.list2.Add(ran_duz[i]);
+            }
+            aVarianti.Text = "A) " + Color1.list2[0];
+            bVarianti.Text = "B) " + Color1.list2[1];
+            cVarianti.Text = "C) " + Color1.list2[2];
+            dVarianti.Text = "D) " + Color1.list2[3];
+            eVarianti.Text = "E) " + Color1.list2[4];
+        }
+
+        private void duymeler(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (this.BackColor.Name.StartsWith(btn.Text.Split(' ')[1]))
+            {
+                //System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Random_Color_Question_App\Correct.wav");
+                //player.Play();
+                MessageBox.Show("Dogrudur.");
+                System.Threading.Thread.Sleep(500);
+                //Environment.Exit(0);
+                Color1.list.Clear();
+                Color1.list2.Clear();
+                this.Form1_Load(null, null);
+                
+            }
+            else
+            {
+                //System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Random_Color_Question_App\Wrong.wav");
+                //player.Play();
+                MessageBox.Show("Yalnisdir!");
+                System.Threading.Thread.Sleep(500);
+                //Environment.Exit(0);
+                Color1.list.Clear();
+                Color1.list2.Clear();
+                this.Form1_Load(null, null);
+            }
+        }
+    }
+    interface Super_Color1
+    {
+        public Color change_color();
+    }
+    class Color1 : Super_Color1
+    {
+        public static List<Color> list = new List<Color>();
+        public static List<string> list2 = new List<string>();
+        public Color1()
+        {
+            list.Add(Color.Yellow);          
+            list.Add(Color.Blue);           
+            list.Add(Color.Black);           
+            list.Add(Color.Red);
+            list.Add(Color.White);  
+        }
+        Random rnd = new Random();
+        public Color change_color()
+        {
+            int rnd_clr_num = rnd.Next(0,5);            
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == rnd_clr_num)
+                {
+                    return list[i];
+                }
+            }
+            return new Color();
+        }
+    }
+}
